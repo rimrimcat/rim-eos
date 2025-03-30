54b815b80a1bc857ad47f5356d7a15adcd7168ca
rimrim <rimrimrimrim1987@gmail.com>
2025-03-30 22:49:18 +0800

    move helper function validateValue to a ts file

--- src/lib/components/GearTable.svelte
+++ src/lib/components/GearTable.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	// Imports
-	import Fuse from 'fuse.js';
 	import { CircleX, CirclePlus } from '@lucide/svelte';
+	import { validateValue } from '../scripts/helpers';
 
 	// Types
 	type ColumnItem = {
@@ -56,73 +56,6 @@
 	// Constants
 	const ICON_SIZE = 20;
 	const ICON_COLOR = '#444';
-
-	const part_map = {
-		helmet: 'Helmet',
-		spaulders: 'Spaulders',
-		armor: 'Armor',
-		armbands: 'Armbands',
-		belt: 'Belt',
-		legguards: 'Legguards',
-		gloves: 'Gloves',
-		boots: 'Boots',
-		visor: 'Visor',
-		eyepiece: 'Visor',
-		engine: 'Engine',
-		combatengine: 'Engine',
-		exoskeleton: 'Exoskeleton',
-		reactor: 'Reactor',
-		microreactor: 'Reactor'
-	} as const;
-
-	const header_map = {
-		// Attack
-		atk: 'Atk',
-		flameatk: 'Flame Atk',
-		frostatk: 'Frost Atk',
-		voltatk: 'Volt Atk',
-		physatk: 'Phys Atk',
-		eleatk: 'Ele Atk',
-		altatk: 'Alt Atk',
-
-		// Attack Percent
-		flameatkpercent: 'Flame Atk %',
-		frostatkpercent: 'Frost Atk %',
-		voltatkpercent: 'Volt Atk %',
-		physatkpercent: 'Phys Atk %',
-		eleatkpercent: 'Ele Atk %',
-		altatkpercent: 'Alt Atk %',
-
-		// Damage Percent
-		flamedmgpercent: 'Flame Dmg %',
-		frostdmgpercent: 'Frost Dmg %',
-		voltdmgpercent: 'Volt Dmg %',
-		physdmgpercent: 'Phys Dmg %',
-		eledmgpercent: 'Ele Dmg %',
-		altdmgpercent: 'Alt Dmg %',
-
-		// Crit
-		crit: 'Crit',
-		critpercent: 'Crit %'
-	} as const;
-
-	const PART = 'part' as 'part';
-	const INTEGER = 'integer' as 'integer';
-	const FLOAT_PERCENT = 'float_percent' as 'float_percent';
-	const HEADER = 'header' as 'header';
-	const INVALID = '???' as '???';
-
-	const validation = {
-		part: PART,
-		atk: INTEGER,
-		ele_atk: INTEGER,
-		total_atk: INTEGER,
-		ele_atk_percent: FLOAT_PERCENT,
-		ele_dmg_percent: FLOAT_PERCENT,
-		crit: INTEGER,
-		crit_percent: FLOAT_PERCENT,
-		header: HEADER
-	} as const;
 
 	function handleDragStart(column: ColumnItem) {
 		draggedColumn = column;
@@ -185,86 +118,6 @@
 		resizingColumn = null;
 		window.removeEventListener('mousemove', handleMouseMove);
 		window.removeEventListener('mouseup', handleMouseUp);
-	}
-
-	function validateValue(col_id: string, inputValue: string): number | string {
-		const validation_type = validation[col_id];
-		console.error('Trying to validate: ' + col_id);
-		console.error('Got validation type');
-		console.error(validation_type);
-
-		if (validation_type) {
-			try {
-				switch (validation_type) {
-					case INTEGER:
-						if (!inputValue.trim()) {
-							return '0';
-						}
-
-						const intValue = parseInt(inputValue, 10);
-
-						if (isNaN(intValue)) {
-							return INVALID;
-						}
-						return String(intValue);
-
-					case FLOAT_PERCENT:
-						if (!inputValue.trim()) {
-							return '0 %';
-						}
-
-						if (isNaN(Number(inputValue))) {
-							return INVALID;
-						}
-						return String(Number(inputValue)) + ' %';
-
-					case PART:
-						if (!inputValue.trim()) {
-							return 'Helmet';
-						}
-
-						const fuse_part = new Fuse(Object.keys(part_map), {
-							includeScore: true,
-							threshold: 0.3,
-							distance: 100
-						});
-
-						const result_part = fuse_part.search(inputValue.trim().toLowerCase());
-						if (result_part.length > 0) {
-							return part_map[result_part[0].item];
-						}
-						return INVALID;
-
-					case HEADER:
-						if (!inputValue.trim()) {
-							return INVALID;
-						}
-
-						const fuse_hdr = new Fuse(Object.keys(header_map), {
-							includeScore: true,
-							threshold: 0.3,
-							distance: 100
-						});
-
-						const result_hdr = fuse_hdr.search(inputValue.trim().toLowerCase());
-						if (result_hdr.length > 0) {
-							return header_map[result_hdr[0].item];
-						}
-
-						return INVALID;
-
-					default:
-						// ???
-						console.error('Default path');
-						return inputValue;
-				}
-			} catch (e) {
-				return INVALID;
-			}
-		}
-
-		console.error('Not validated.');
-		return inputValue;
 	}
 
 	function startEditCell(row: RowItem, column: ColumnItem) {
