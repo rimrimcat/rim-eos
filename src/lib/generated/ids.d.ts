c6098b884bf292c7fd5268a1110bbbddfc4bbe55
rimrim <rimrimrimrim1987@gmail.com>
2025-04-12 19:15:30 +0800

    add Id auto generator

---
+++ src/lib/generated/ids.d.ts
@@ -0,0 +1,23 @@
+// Auto-generated file
+
+export type EffectsIds =
+	| 'grayfox-a3-buff'
+	| 'grayfox-a3-debuff'
+	| 'grayfox-a6'
+	| 'grayfox-skill'
+	| 'nanyin-a5'
+	| 'nanyin-a6'
+	| 'nanyin-inhabit'
+	| 'nola-a3'
+	| 'nola-a6'
+	| 'nola-frost-skill';
+export type ResoEffectsIds =
+	| 'atk-reso'
+	| 'bene-reso'
+	| 'fiona-reso'
+	| 'flame-reso'
+	| 'fort-reso'
+	| 'frost-reso'
+	| 'nanyin-reso'
+	| 'phys-reso'
+	| 'volt-reso';
---
+++ src/lib/scripts/id-type-gen.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+
+const projectRoot = process.cwd();
+const out_file = path.join(projectRoot, 'src/lib/generated/ids.d.ts');
+
+const effects_path = path.join(projectRoot, 'static/json/effects');
+const reso_effects_path = path.join(projectRoot, 'static/json/reso_effects');
+const weapons_path = path.join(projectRoot, 'static/json/weapons');
+
+const effects_ids = fs
+	.readdirSync(effects_path)
+	.filter((file) => file.endsWith('.json'))
+	.map((file) => path.basename(file, '.json'));
+const reso_effects_ids = fs
+	.readdirSync(reso_effects_path)
+	.filter((file) => file.endsWith('.json'))
+	.map((file) => path.basename(file, '.json'));
+const weapons_ids = fs
+	.readdirSync(weapons_path)
+	.filter((file) => file.endsWith('.json'))
+	.map((file) => path.basename(file, '.json'));
+
+const typeDef = `// Auto-generated file
+
+export type EffectsIds = ${effects_ids.map((id) => `"${id}"`).join(' | ')};
+export type ResoEffectsIds = ${reso_effects_ids.map((id) => `"${id}"`).join(' | ')};
+
+`;
+
+fs.writeFileSync(out_file, typeDef);
+console.log('Id types generated.');
