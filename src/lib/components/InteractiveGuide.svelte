773ce202cf9fb7bc3b75391543e5507bea60b0c9
rimrim <rimrimrimrim1987@gmail.com>
2025-04-18 23:19:16 +0800

    add component for interactive guide (will just export stuff)

---
+++ src/lib/components/InteractiveGuide.svelte
@@ -0,0 +1,18 @@
+<script module>
+	import type { Snippet } from 'svelte';
+	// @ts-expect-error
+	export const GUIDE_GEAR_ELEMENT: (Snippet | string)[] = [gear_element_req, 'some string'];
+</script>
+
+<script lang="ts">
+</script>
+
+{#snippet gear_element_req()}
+	<p>In order to get started, we will need the following:</p>
+	<ul>
+		<li>Loadout (Weapon, Matrix, Traits, Relics)</li>
+		<li>Equipped Gears</li>
+		<li>Character stats (including shown base attack stats)</li>
+	</ul>
+	<p>If you do not have access to them, then you might not be able to complete the tutorial.</p>
+{/snippet}
