e2aca5036f07dc10323a2d4df58775790cb94559
rimrim <rimrimrimrim1987@gmail.com>
2025-04-18 23:40:39 +0800

    define GuideItem type

---
+++ src/lib/types/nav-types.d.ts
@@ -0,0 +1,11 @@
+import type { Snippet } from 'svelte';
+import type { Writable } from 'svelte/store';
+
+export type GuideItem = {
+	snippet?: Snippet; // snippet to render
+	text?: string; // text to display
+
+	move_to?: { x: number; y: number }; // move window position
+	disable_next?: boolean; // disable next button
+	proceed_on?: Writable<boolean>; // proceed when this writable is true
+};
