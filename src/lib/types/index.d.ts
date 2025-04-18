220a428de945ff147c00968ba5fd372888890d31
rimrim <rimrimrimrim1987@gmail.com>
2025-04-18 23:40:51 +0800

    add re-export from index

--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -2,5 +2,6 @@ export * from '../generated/ids';
 export * from './effect-types';
 export * from './gear-types';
 export * from './loadout-types';
+export * from './nav-types';
 export * from './stat-types';
 export * from './weapon-types';
