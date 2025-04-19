dcd79510bb6f349ad16f270793ebd3ed0a29b661
rimrim <rimrimrimrim1987@gmail.com>
2025-04-19 11:11:24 +0800

    allow move_to values to be either string or number

--- src/lib/types/nav-types.d.ts
+++ src/lib/types/nav-types.d.ts
@@ -5,7 +5,7 @@ export type GuideItem = {
 	snippet?: Snippet; // snippet to render
 	text?: string; // text to display
 
-	move_to?: { x: number; y: number }; // move window position
+	move_to?: { x: number | string; y: number | string }; // move window position
 	disable_next?: boolean; // disable next button
 	proceed_on?: Writable<boolean>; // proceed when this writable is true
 };
